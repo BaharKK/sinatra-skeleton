aa4fcd8b4bb344b1f82fefe4e3f3995687cebeb4
BaharKK <bahar_rachapalli@yahoo.com>
2016-01-24 23:57:37 +0000

    Remove title field and Add url field

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
+require 'uri'
 class Message < ActiveRecord::Base 
   validates :author, presence: true 
-  validates :title, presence: true
   validates :content, presence: true, length: {minimum: 20}
+  validates :url, :format => URI::regexp(%w(http https)), allow_blank: true 
+
 end
