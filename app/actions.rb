cd8f02611b5296a494532ce62db4b02d45a70877
BaharKK <bahar_rachapalli@yahoo.com>
2016-01-24 09:40:18 +0000

    Validation(part A)

--- app/actions.rb
+++ app/actions.rb
@@ -23,6 +23,9 @@ end
       content: params[:content],
       author: params[:author]
     )
-    @message.save
-    redirect '/messages'
+    if @message.save
+       redirect '/messages'
+    else
+      erb :'messages/new'
+    end
   end
